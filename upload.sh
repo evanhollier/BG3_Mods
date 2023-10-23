a3a7331fa08e30dc45ddeb5b3cfaf0ef4bfd6b0d
evanhollier <38679400+evanhollier@users.noreply.github.com>
2023-10-23 04:51:23 -0600

    Delete upload.sh

--- upload.sh
+++
@@ -1,8 +0,0 @@
-git add .
-
-commit_message=$(git log --oneline -1 --pretty=%B)
-git commit -m "$commit_message"
-
-git remote add origin git@github.com:evanhollier/BG3_Mods.git
-
-git push -u origin master
