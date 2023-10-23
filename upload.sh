423ffe35e39ea648d3a7cc4e4667a78188865e1a
Evan Hollier <evan.hollier98@gmail.com>
2023-10-22 21:45:52 -0600

    second commit

--- upload.sh
+++ upload.sh
@@ -3,6 +3,6 @@ git add .
 commit_message=$(git log --oneline -1 --pretty=%B)
 git commit -m "$commit_message"
 
-@REM git remote add origin git@github.com:evanhollier/BG3_Mods.git
+git remote add origin git@github.com:evanhollier/BG3_Mods.git
 
 git push -u origin master
